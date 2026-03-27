df67ebcc8b51bcd36dbb94ddd9e105d3c551fe91
hi991016 <miureus@gmail.com>
2026-03-27 11:14:18 +0700

    commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { readdirSync, existsSync } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// Fix __dirname cho ESM
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+const pages: Record<string, string> = {
+  main: resolve(__dirname, "index.html"),
+};
+
+readdirSync(__dirname, { withFileTypes: true })
+  .filter(
+    (d) =>
+      d.isDirectory() && !["node_modules", "public", "src", "dist"].includes(d.name),
+  )
+  .forEach((d) => {
+    const htmlPath = resolve(__dirname, d.name, "index.html");
+    if (existsSync(htmlPath)) {
+      pages[d.name] = htmlPath;
+    }
+  });
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: { input: pages },
+  },
+});
